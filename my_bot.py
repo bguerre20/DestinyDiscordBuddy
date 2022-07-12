1081d51079119ac6be0879652ee8132a54e1b7a4
Roguekiller <Brantley652@gmail.com>
2022-07-12 12:43:49 -0400

    added skeleton for discord bot

--- my_bot.py
+++ my_bot.py
@@ -1,11 +1,20 @@
+# bot.py
+import os
+
 #import discord package
 import discord
+
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 # client(out bot)
 client = discord.Client()
 
-# do stuff...
-
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
 
 # run the client on the server
-client.run()
+client.run(TOKEN)
