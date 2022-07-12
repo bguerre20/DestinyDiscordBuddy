cca759a7dff80751dca8fd27f8aa49781055ae45
Roguekiller <Brantley652@gmail.com>
2022-07-12 12:52:19 -0400

    comments added

--- my_bot.py
+++ my_bot.py
@@ -12,6 +12,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 # client(out bot)
 client = discord.Client()
 
+# asnyc event
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
